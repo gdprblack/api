fcf05023cc25d47877d42ec5c6ca200d74f482a6
Sergi Sanchez Deutsch <s.s.deutsch72@gmail.com>
2018-10-21 01:45:56 -0700

    blockchain API integration

--- src/controllers/data.controller.ts
+++ src/controllers/data.controller.ts
@@ -1,6 +1,7 @@
 import DataModel from "../models/data.model";
 import { Request, Response } from "express";
 import { encryptData, decryptKey, decryptData } from "@gdprblack/secrets";
+import { addEvent, deployNewContract, getLogList, getLogData } from "@gdprblack/blockchain";
 import Entity from "../controllers/entity.controller";
 import User from "../controllers/user.controller";
 import { UserRoles } from "../constants";
@@ -13,13 +14,34 @@ class DataController {
         result.entityId = req.body.entity;
         result.dbId = req.body.id;
 
-        const newData  = new Data.model(result);
+        const newData = new Data.model(result);
 
         newData.save((err, data) => {
             if (err) {
                 return res.status(500).send(err);
             }
             res.status(201).json(data);
+        });
+    }
+
+    public newDataObject(req: Request, res: Response) {
+        const result = deployNewContract(req.body.mongoid);
+        Promise.all([result]).then((values) => {
+            res.status(201).json(values);
+        });
+    }
+
+    public addEvent(req: Request, res: Response) {
+        const result = addEvent(req.body.address, req.body.timestamp, req.body.user, req.body.type, req.body.metadata);
+        Promise.all([result]).then((values) => {
+            res.status(201).json(values);
+        });
+    }
+
+    public getLogList(req: Request, res: Response) {
+        const result = getLogList(req.body.address);
+        Promise.all([result]).then((values) => {
+            res.status(201).json(values);
         });
     }
 
@@ -42,7 +64,7 @@ class DataController {
 
     public async signRequest(req: Request, res: Response) {
         const data: any = await Data.model.findById(req.params.id);
-        const user: any = await User.model.findOne({publicKey: req.body.publicKey});
+        const user: any = await User.model.findOne({ publicKey: req.body.publicKey });
         const members = await Entity.controller.getEntityUsers(data!.entityId);
         if (user.role === UserRoles.BoardMember) {
             const result = decryptKey(data.encryptedKeys.boardMembers[user.publicKey], req.body.privateKey);
